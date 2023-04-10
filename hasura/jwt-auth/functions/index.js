fa353ccd298004a9ca7ea61423483cb60edba141
@kaspar <team@crewnew.com>
2023-04-10 15:30:01 +0100

    Update JWT auth

--- hasura/jwt-auth/functions/index.js
+++ hasura/jwt-auth/functions/index.js
@@ -4,23 +4,24 @@ const { request } = require('graphql-request');
 
 admin.initializeApp();
 
+// Add your API endpoint and admin secret here
 const HASURA_GRAPHQL_ENDPOINT = 'https://cloud-services.hasura.app/v1/graphql';
 const HASURA_ADMIN_SECRET = 'wIwO9JgznnaqmN4rAoc9fTUvJifxj8iOuh8Uec5anaclbtupTIfrGG7z7gQoZEJ1';
 
 const insertUserQuery = `
-  mutation insertUser($uid: String!, $email: String!, $first_name: String, $photo_url: String, $mobile_number: String, $role: String!, $created_at: timestamptz!) {
-    insert_users_one(object: {uid: $uid, email: $email, first_name: $first_name, photo_url: $photo_url, mobile_number: $mobile_number, role: $role, created_at: $created_at}) {
+  mutation insertUser($uid: String!, $email: String!, $first_name: String, $role: String!, $created_at: timestamptz!) {
+    insert_users_one(object: {uid: $uid, email: $email, first_name: $first_name, role: $role, created_at: $created_at}) {
       id
     }
   }
 `;
 
 exports.processSignUp = functions.auth.user().onCreate(async (user) => {
-    // Set custom claims with the "customer" role
+    // Set custom claims with the "client" role
     await admin.auth().setCustomUserClaims(user.uid, {
         'https://hasura.io/jwt/claims': {
-            'x-hasura-default-role': 'customer',
-            'x-hasura-allowed-roles': ['customer']
+            'x-hasura-default-role': 'client',
+            'x-hasura-allowed-roles': ['client']
         }
     });
 
@@ -29,9 +30,7 @@ exports.processSignUp = functions.auth.user().onCreate(async (user) => {
         uid: user.uid,
         email: user.email,
         first_name: user.displayName,
-        photo_url: user.photoURL,
-        mobile_number: user.phoneNumber,
-        role: 'customer',
+        role: 'client',
         created_at: new Date().toISOString()
     };
 
