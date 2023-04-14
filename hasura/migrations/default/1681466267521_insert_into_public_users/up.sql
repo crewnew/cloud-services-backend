cde73b4501005a5346d99a0346a6f4cc34750be3
martinmakejev <60092537+martinmakejev@users.noreply.github.com>
2023-04-14 12:58:40 +0300

    Made user migration

---
+++ hasura/migrations/default/1681466267521_insert_into_public_users/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."users"("verified", "company_id", "id", "email", "first_name", "last_name", "role", "created_at", "updated_at") VALUES (true, 1, 18, E'jan.kangi@tpt.tp', E'Jan', E'Kangi', E'worker', E'2023-04-14T09:57:47.262698+00:00', E'2023-04-14T09:57:47.262698+00:00');
