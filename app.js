cb504add681a61165409cd2bb259af9c8f136075
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2023-04-21 12:43:51 +0300

    Styling & notices

--- app.js
+++ app.js
@@ -16,14 +16,38 @@ firebase.initializeApp(firebaseConfig);
 // Register and login
 
 const auth = firebase.auth();
+
+function deletableElements() {
+	const deletableElements = document.getElementsByTagName("p");
+	while(deletableElements.length > 0){
+		deletableElements[0].parentNode.removeChild(deletableElements[0]);
+	}
+}
+
 async function register() {
 	const email = document.getElementById("email").value;
 	const password = document.getElementById("password").value;
 	try {
 		const userCredential = await auth.createUserWithEmailAndPassword(email, password);
+		
+		deletableElements();
+
+		const successElement = document.createElement("p");
+		const successText = document.createTextNode("User created successfully");
+		successElement.classList.add("success");
+		successElement.appendChild(successText);
+		const element = document.getElementById("login")
+		element.appendChild(successElement);
 		console.log("User created successfully", userCredential);
 	} catch (error) {
-		console.log("Error while creating user", error);
+		deletableElements();
+
+		const errorElement = document.createElement("p");
+		const errorText = document.createTextNode(error);
+		errorElement.classList.add("error");
+		errorElement.appendChild(errorText);
+		const element = document.getElementById("login")
+		element.appendChild(errorElement);
 	}
 }
 
@@ -33,8 +57,24 @@ async function login() {
 	try {
 		const userCredential = await auth.signInWithEmailAndPassword(email, password);
 		const jwttoken = await userCredential.user.getIdToken();
+		deletableElements();
+
+		const successElement = document.createElement("p");
+		const successText = document.createTextNode("User logged in successfully");
+		successElement.classList.add("success");
+		successElement.appendChild(successText);
+		const element = document.getElementById("login")
+		element.appendChild(successElement);
 		console.log("User logged in successfully", jwttoken);
 	} catch (error) {
+		deletableElements();
+
+		const errorElement = document.createElement("p");
+		const errorText = document.createTextNode(error);
+		errorElement.classList.add("error");
+		errorElement.appendChild(errorText);
+		const element = document.getElementById("login")
+		element.appendChild(errorElement);
 		console.log("Error while logging in user", error);
 	}
 }
