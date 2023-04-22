3178986a1fcfdb5636e079b4ebdb703ad13dc06b
martinmakejev <60092537+martinmakejev@users.noreply.github.com>
2023-04-22 13:24:12 +0300

    Delete hasura/migrations/default/1681467958132_<name> directory

--- hasura/migrations/default/1681467958132_<name>/down.sql
+++
@@ -1,4 +0,0 @@
-
-DELETE FROM "public"."invoices" WHERE "id" = 6;
-
-DELETE FROM "public"."users" WHERE "id" = 21;
--- hasura/migrations/default/1681467958132_<name>/up.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO "public"."users"("verified", "company_id", "id", "email", "first_name", "last_name", "role", "created_at", "updated_at") VALUES (true, 1, 21, E'jan.kangi@tpt', E'Jan', E'Kangi', E'worker', E'2023-04-14T10:24:59.924345+00:00', E'2023-04-14T10:24:59.924345+00:00');
-
-INSERT INTO "public"."invoices"("date", "company_id", "id", "total", "status", "created_at", "updated_at") VALUES (E'2023-04-14', 1, 6, 500, E'han', E'2023-04-14T10:25:57.904992+00:00', E'2023-04-14T10:25:57.904992+00:00');
